a08acff68c469df448b90de53fb86407a2953244
Kaleidoscope39 <59548572+Kaleidoscope39@users.noreply.github.com>
2025-03-19 23:46:34 +0800

    diypart: remove mosdns.

--- diy.sh
+++ diy.sh
@@ -41,9 +41,6 @@ svn_export "master" "applications/luci-app-smartdns" "feeds/luci/applications/lu
 svn_export "master" "net/smartdns" "feeds/packages/net/smartdns" "https://github.com/immortalwrt/packages"
 svn_export "master" "applications/luci-app-zerotier" "feeds/luci/applications/luci-app-zerotier" "https://github.com/immortalwrt/luci"
 svn_export "master" "net/zerotier" "feeds/packages/net/zerotier" "https://github.com/immortalwrt/packages"
-svn_export "v5" "luci-app-mosdns" "package/luci-app-mosdns" "https://github.com/sbwml/luci-app-mosdns"
-svn_export "v5" "mosdns" "package/mosdns" "https://github.com/sbwml/luci-app-mosdns"
-svn_export "v5" "v2dat" "package/v2dat" "https://github.com/sbwml/luci-app-mosdns"
 
 # 编译 po2lmo (如果有po2lmo可跳过)
 #pushd package/luci-app-openclash/tools/po2lmo
